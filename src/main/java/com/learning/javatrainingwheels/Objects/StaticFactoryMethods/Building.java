c49c1f806aca77770191caa9786b7d0bb969f5b7
Daniel Dorsey <danieldorsey472@gmail.com>
2024-09-15 21:31:10 -0400

    Add building class

---
+++ src/main/java/com/learning/javatrainingwheels/Objects/StaticFactoryMethods/Building.java
@@ -0,0 +1,8 @@
+package com.learning.javatrainingwheels.Objects.StaticFactoryMethods;
+
+// Static factory methods are used to create new objects as an alternative to constructors.
+// They're not mutually exclusive, you CAN have static factory methods AND public constructors if you'd like
+public class Building {
+    // TODO: Create static factory methods to be able to instantiate new objects that users can use instead of a constructor
+    // TODO: Create methods to be able to create a fresh basic building, to create a building based on a string input parameter, and based on a series of expected props.
+}
