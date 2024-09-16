f8359d4eb1df4807ac019c6f91dd6cf01e0b8aae
Daniel Dorsey <danieldorsey472@gmail.com>
2024-09-15 20:57:55 -0400

    init commit

---
+++ src/main/java/com/learning/javatrainingwheels/JavaTrainingWheelsApplication.java
@@ -0,0 +1,13 @@
+package com.learning.javatrainingwheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrainingWheelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTrainingWheelsApplication.class, args);
+    }
+
+}
